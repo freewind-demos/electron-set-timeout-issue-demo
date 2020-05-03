6c4502fac655b9460e140a2d91e165e0cff855c8
Freewind <nowindlee@gmail.com>
2020-05-03 17:28:47 +0800

    add slow task

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-Electron Hello World Demo
+Electron Set Timeout Demo
 =========================
+
+测试在Electron下`setTimeout`是否正常运行。
+
+之前在某处发现`setTimeout(()=>{}, 1)`与`setTimeout(()=>{}, 200)`实际上是几乎同时执行的，
+不过在这个测试中没有发现该问题。可能是electron版本或者别的原因。
+
+发现另一种可能：队列中已经有了大量的任务，结果有一个任务特别耗时，把后面的任务都挤到一起去了。
 
 ```
 npm install
 npm start
 ```
-
-![demo](./images/demo.jpg)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-document.getElementById("content").innerText = "hello from javascript";
+function show(line) {
+  document.getElementById("content").innerHTML += `<div>${line}</div>`;
+}
+
+function slowTask(ms) {
+  const start = Date.now();
+  while (Date.now() - start <= ms) {
+  }
+}
+
+function testTimeout(delay) {
+  const start = Date.now()
+  setTimeout(() => {
+    const now = Date.now();
+    if (delay === 10) {
+      slowTask(5000);
+    }
+    show(`start: ${start}, now: ${now}, expected delay: ${delay}, real delay: ${now - start}`)
+  }, delay);
+}
+
+for (let i = 0; i < 10000; i += 10) {
+  testTimeout(i);
+}
